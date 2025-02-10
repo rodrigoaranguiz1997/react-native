a6ced0b104271f5f0f002c4a12684059750dec59
rodrigoaranguiz1997 <rodrigo.a.aranguiz@accenture.com>
2025-02-10 19:40:03 -0300

    sprint: 3 Rodrigo Aránguiz

--- src/screens/CityScreen/CityScreen.tsx
+++ src/screens/CityScreen/CityScreen.tsx
@@ -1,11 +1,13 @@
 import CityCard from '@/components/cityCard';
 import React from 'react'
 import { View, Text, FlatList } from "react-native";
+import {router} from "expo-router"
 
 const CityScreen = () => {
     const onPress = (name: string) => {
         alert("Redireccion a ruta dinamica");
-        window.location.href = "/cityDetail?name="+name;
+        //window.location.href = "/cityDetail?name="+name;
+        router.push( "/cityDetail?name="+name);
     }
     const data = [{ name: "ciudad", image: "https://testimage.com/city" }];
     return (
